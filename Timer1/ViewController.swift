e73e5caff205de96e48626ef64ab9b17445fbf26
Роберт Эдуардович <robert@macbook-pro-robert.local>
2019-03-17 20:33:39 +0500

    Initial Commit

---
+++ Timer1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer1
+//
+//  Created by Роберт Райсих on 17/03/2019.
+//  Copyright © 2019 Роберт Райсих. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Timer1Tests/Timer1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Timer1Tests.swift
+//  Timer1Tests
+//
+//  Created by Роберт Райсих on 17/03/2019.
+//  Copyright © 2019 Роберт Райсих. All rights reserved.
+//
+
+import XCTest
+@testable import Timer1
+
+class Timer1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
